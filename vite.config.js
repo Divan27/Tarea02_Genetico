4251aa2fcdc89c2c3b39117968d2d114fb42aea2
Roymar Castillo <roymarcastillo@estudiantec.cr>
2025-10-17 20:31:46 -0600

    Correcion del vite.config.js para el deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Divan27/Tarea02_Genetico",
+  base: "/Tarea02_Genetico/",
   plugins: [react()],
 })
