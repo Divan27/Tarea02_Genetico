dfb4ebf1de12851b6e20eed6c164447a12b95942
Roymar Castillo <roymarcastillo@estudiantec.cr>
2025-10-17 22:38:29 -0600

    Arreglar errores de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
